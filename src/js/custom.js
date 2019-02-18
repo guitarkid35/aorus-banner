fb677657ac8a485c17d35a1a296900c2d8890e0b
Garrick Chou <guitarkid35@gmail.com>
2019-02-18 10:02:13 +0800

    index: add slide 2 HUD UI

--- src/js/custom.js
+++ src/js/custom.js
@@ -12,16 +12,19 @@ var cliplight = TweenMax.to(".slide-inner--item_light",2,{
 timeline.set(".slide-inner--item_screen",{
   transformOrigin: "50% 50%"
 })
-var clipscreen = TweenMax.to(".slide-inner--item_screen",3,{
-  css: { opacity:"1", transform: "scale(1)"}
+var clipscreen = TweenMax.to(".slide-inner--item_screen",2,{
+  css: { opacity:"1", transform: "scale(1)"},
+  ease: Expo.easeOut
+
 })
-var clipbg = TweenMax.to(".slide-inner--item_bg",3,{
+var clipbg = TweenMax.to(".slide-inner--item_bg",2,{
   // opacity: 1
-  css: { opacity:"1", transform: "scale(1)"}
+  css: { opacity:"1", transform: "scale(1)"},
+  ease: Expo.easeOut
 })
 
 timeline.add(clipeye,1)
 timeline.add(cliplens,0.5)
 timeline.add(cliplight, 1.8)
-timeline.add(clipscreen, 3)
-timeline.add(clipbg, 3)
+timeline.add(clipscreen, 2.8)
+timeline.add(clipbg, 2.8)
